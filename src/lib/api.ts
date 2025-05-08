3fa5de202e046f6a2e90fb6a19373bf1a5428270
Krish Soni <isonikrish@gmail.com>
2025-05-08 18:33:03 +0530

    fix: build error

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,7 +10,7 @@ export async function getUser(username: string) {
       }api/profile/get-user?username=${username}`
     );
     return res.data;
-  } catch (error) {
+  } catch {
     return null;
   }
 }
