cfb48833de1c4ba21e03db772fcfcf0844401aa9
Krish Soni <isonikrish@gmail.com>
2025-05-08 19:13:38 +0530

    fix: build error

--- src/app/(landing)/[username]/page.tsx
+++ src/app/(landing)/[username]/page.tsx
@@ -1,15 +1,19 @@
 import { getUser } from "@/lib/api";
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
 import UserProfile from "./_components/UserProfile";
+import { NextPage } from "next";  // Import NextPage
 
 interface Props {
-    params: { username: string };
+  params: { username: string };
 }
 
-export default async function UserProfilePage({ params }: Props){
-    const { username } = params; 
-    const user = await getUser(username);
-    if(!user) return notFound() ;
+const UserProfilePage: NextPage<Props> = async ({ params }) => {
+  const { username } = params;
+  const user = await getUser(username);
 
-    return <UserProfile user={user}/>
-}
+  if (!user) return notFound();
+
+  return <UserProfile user={user} />;
+};
+
+export default UserProfilePage;
