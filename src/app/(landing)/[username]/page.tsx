19e65d6b0a93d85624f90792a56b2a0fce4a495b
Krish Soni <isonikrish@gmail.com>
2025-05-08 19:09:03 +0530

    fix: build error

--- src/app/(landing)/[username]/page.tsx
+++ src/app/(landing)/[username]/page.tsx
@@ -1,33 +1,15 @@
-
+import { getUser } from "@/lib/api";
 import { notFound } from 'next/navigation';
-import { getUser } from '@/lib/api';
-import UserProfile from './_components/UserProfile';
-import { UserType } from '@/lib/types';
+import UserProfile from "./_components/UserProfile";
 
-export async function generateStaticParams() {
-  // Fetch the usernames from your backend (e.g., API or database)
-  const response = await fetch(`${process.env.NODE_ENV === "production"
-      ? "https://portly.netlify.app/"
-      : "http://localhost:3000/"
-    }api/fetch-users`);
-  const users: UserType[] = await response.json();
-
-  const usernames = users.map((user) => user.username);
-
-  return usernames.map((username) => ({ username }));
+interface Props {
+    params: { username: string };
 }
 
+export default async function UserProfilePage({ params }: Props){
+    const { username } = await params; 
+    const user = await getUser(username);
+    if(!user) return notFound() ;
 
-type Props = {
-  params: {
-    username: string;
-  };
-};
-
-export default async function UserProfilePage({ params }: Props) {
-  const user = await getUser(params.username);
-
-  if (!user) return notFound();
-
-  return <UserProfile user={user} />;
+    return <UserProfile user={user}/>
 }
